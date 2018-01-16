8e04a95ce3d456b28be4cb9fcfcbf7aed59448db
Jesse Hansen <twindagger@gmail.com>
2018-01-16 10:41:33 -0700

    Add prepend and append

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const filters = require('./lib')
 
-const pipeFactory = (...pipeFilters) => {
-  const pwrap = (fn, ...args) => {
-    let res
-    try {
-      res = fn(...args)
-    } catch (ex) {
-      return Promise.reject(ex)
-    }
-    if (res && typeof (res.then) === 'function') {
-      return res
-    }
-    return Promise.resolve(res)
+const pwrap = (fn, ...args) => {
+  let res
+  try {
+    res = fn(...args)
+  } catch (ex) {
+    return Promise.reject(ex)
   }
+  if (res && typeof (res.then) === 'function') {
+    return res
+  }
+  return Promise.resolve(res)
+}
+
+const pipeFactory = (...pipeFilters) => {
+  const filters = [].concat(pipeFilters)
 
   return {
     send (context) {
       const step = (idx) => (ctx) =>
-        idx < pipeFilters.length
-          ? pwrap(pipeFilters[idx], ctx, step(idx + 1))
+        idx < filters.length
+          ? pwrap(filters[idx], ctx, step(idx + 1))
           : Promise.resolve(ctx)
 
       return step(0)(context)
     },
+    prepend (filtersToPrepend) {
+      return pipeFactory(filtersToPrepend.concat(pipeFilters))
+    },
+    append (filtersToAppend) {
+      return pipeFactory(pipeFilters.concat(filtersToAppend))
+    },
     inspect () {
       return {
-        pipeline: pipeFilters.map((m) => typeof (m.inspect) === 'function' ? m.inspect() : 'Unknown Filter')
+        pipeline: filters.map((m) => typeof (m.inspect) === 'function' ? m.inspect() : 'Unknown Filter')
       }
     }
   }
